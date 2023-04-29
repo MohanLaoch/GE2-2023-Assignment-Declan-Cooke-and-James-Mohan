afdffdefa67649a2022c8b2fdd5d490123508210
declanqc <C20465442@mytudublin.ie>
2023-04-29 18:45:57 +0100

    added welkir the wanderer

--- AquariumSIM/Assets/Scripts/SteeringBehaviour.cs
+++ AquariumSIM/Assets/Scripts/SteeringBehaviour.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Boid))]
+[RequireComponent(typeof(Boid1))]
 public abstract class SteeringBehaviour : MonoBehaviour
 {
     public float weight = 1.0f;
     public Vector3 force;
 
     [HideInInspector]
-    public Boid boid;
+    public Boid1 boid;
 
     public void Awake()
     {
-        boid = GetComponent<Boid>();
+        boid = GetComponent<Boid1>();
     }
 
     public abstract Vector3 Calculate();
