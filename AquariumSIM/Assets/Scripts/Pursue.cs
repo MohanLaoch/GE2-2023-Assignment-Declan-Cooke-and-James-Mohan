44e78bb0cab2ac97327c2e601e5fbc2ba3fc5dec
James Mohan <c20305011@mytudublin.ie>
2023-04-30 15:09:29 +0100

    removed boid 1

--- AquariumSIM/Assets/Scripts/Pursue.cs
+++ AquariumSIM/Assets/Scripts/Pursue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pursue : MonoBehaviour
 {
-    public Boid1 target;
+    public Boid target;
     // Start is called before the first frame update
     void Start()
     {
