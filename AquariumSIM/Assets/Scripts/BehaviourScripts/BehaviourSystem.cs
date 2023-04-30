95d873083bd8a1627f2a9650638d6d35395be9c2
James Mohan <c20305011@mytudublin.ie>
2023-04-30 15:46:14 +0100

    organisation

---
+++ AquariumSIM/Assets/Scripts/BehaviourScripts/BehaviourSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BehaviourSystem : StateMachine
+{
+    private void Start()
+    {
+        
+    }
+}
